96ea1cb7225a4917c38ac90d7e54215d332a563c
tmcmac <timmcmackenjr@gmail.com>
2024-06-25 17:01:26 -0600

    Unit 6 part 1 switch statements

--- Switch/src/Main.java
+++ Switch/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int value = 3;
+        int value = 11;
 
         if (value ==1) {
             System.out.println("Value is one!");
@@ -11,14 +11,37 @@ public class Main {
         }
 
         switch (value) {
-            case 1:
-                System.out.print("The value is one");
-                break;
-            case 2:
-                System.out.println("The value is two");
-                break;
+            case 1 -> System.out.print("The value is one");
+            case 2 -> System.out.println("The value is two");
+            case 3, 4, 5 -> System.out.println("The value is " + value);
+            default -> System.out.println("The value is greater than 5");
+        }
+
+        String month = "MAY";
+        System.out.println("The month of " + month + " is in " + getQuarter(month));
+
+
+    }
+    public static String getQuarter(String month) {
+        switch (month) {
+            case "JANUARY":
+            case "February":
+            case "MARCH":
+                return ("quarter 1");
+            case "APRIL":
+            case "MAY":
+            case "JUNE":
+                return ("quarter 2");
+            case "JULY":
+            case "AUGUST":
+            case "SEPTEMBER":
+                return ("quarter 3");
+            case "OCTOBER":
+            case "NOVEMBER":
+            case "DECEMBER":
+                return ("quarter 4");
             default:
-                System.out.println("The value is not one or two");
+                return ("Unknown...");
         }
     }
 }
