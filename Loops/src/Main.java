a53f94fb9ea10c7ace9d8f4e14137cf28a89fab1
tmcmac <timmcmackenjr@gmail.com>
2024-06-25 18:40:33 -0600

    Cleaned up for loop 1 and commented out tests

--- Loops/src/Main.java
+++ Loops/src/Main.java
@@ -6,14 +6,14 @@ public class Main {
 //        }
 
         // Test a few calls to calcInterest
-        System.out.println("$100 at 5% interest -> $" + calcInterest(100, 5));
-        System.out.println("$1000 at 15% interest -> $" + calcInterest(1000, 15));
-        System.out.println("$25 at 95% interest -> $" + calcInterest(25, 95));
-        System.out.println("$11 at 500% interest -> $" + calcInterest(11, 500));
+//        System.out.println("$100 at 5% interest -> $" + calcInterest(100, 5));
+//        System.out.println("$1000 at 15% interest -> $" + calcInterest(1000, 15));
+//        System.out.println("$25 at 95% interest -> $" + calcInterest(25, 95));
+//        System.out.println("$11 at 500% interest -> $" + calcInterest(11, 500));
 
         // Test a for loop for increasing interest
         for (double interest = 3.0; interest <= 8.0; interest++) {
-            System.out.println("$100000 with " + interest + "% interests -> " + calcInterest(100000, interest));
+            System.out.println("$1000000 with " + interest + "% interests -> " + calcInterest(1000000, interest));
         }
 
 
