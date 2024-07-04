21b74e45a39d0707eb12cd598824b730c1bd2d2f
tmcmac <timmcmackenjr@gmail.com>
2024-07-04 12:29:51 -0700

    Adds validation to some setters

--- ClassesPart1/src/Car.java
+++ ClassesPart1/src/Car.java
@@ -5,6 +5,53 @@ public class Car {
     private int doors = 4;
     private boolean convertible = false;
 
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
+
+    public void setMake(String make) {
+        if(make == null) make = "Unknown";
+        String lowercaseMake = make.toLowerCase();
+        switch (lowercaseMake) {
+            case "ford", "chevrolet", "gm", "chrysler", "toyota", "mercedes", "porsche", "tesla" -> this.make = make;
+            default -> {
+                this.make = "Unsupported";
+            }
+        }
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
+    }
+
     public void describeCar() {
         System.out.println( doors + "-Door " + color + " " + make + " " + model + " " + (convertible ? "Convertible" : ""));
     }
--- ClassesPart1/src/Main.java
+++ ClassesPart1/src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
     public static void main(String[] args) {
+
+        // instantiate an object of type car or else get a null pointer reference
         Car car = new Car();
+
+        // show the default values we have for car in the car class
+        System.out.println("Default values: ");
+        System.out.println("Make = " + car.getMake());
+        System.out.println("Model = " + car.getModel());
+
+        // now lets make a dream car
+        System.out.println("\nDream car: ");
+        car.setMake("Mercedes");
+        car.setModel("Mclaren SLR");
+        car.setDoors(2);
+        car.setColor("Red");
+        car.setConvertible(false);
+        car.describeCar();
+
+        // now lets test some validation in the make setter
+        System.out.println("\nTest unsupported data: ");
+        car.setMake("Hyundai");
         car.describeCar();
     }
 }
