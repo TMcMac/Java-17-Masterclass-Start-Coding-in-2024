4eb6ba9e18e66942cc500b5aa065ae0700f9efd4
tmcmac <timmcmackenjr@gmail.com>
2024-07-15 17:08:18 -0400

    Inheritence challenge part 1

---
+++ InheritenceChallenge1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Worker testWorker = new Worker("John", "01/01/1990", "07/15/2024");
+        System.out.println(testWorker.name + " is " + testWorker.getAge() + " years old.");
+        System.out.println(testWorker.name + " was paid " + testWorker.());
+
+    }
+}
---
+++ InheritenceChallenge1/src/Worker.java
@@ -0,0 +1,29 @@
+public class Worker {
+    //Top class for employees of a business
+    private String name;
+    private String dateOfBirth;
+    private String endDate;
+
+    public Worker(String name, String dateOfBirth, String endDate) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.endDate = endDate;
+    }
+
+    public int getAge() {
+        if (this.dateOfBirth.length() < 8) {
+            System.out.println("Date of birth was not entered as mm/dd/yyyy");
+            return -1;
+        }
+        String subStr = this.dateOfBirth.substring(this.dateOfBirth.length(), -4);
+        return (2024 - Integer.parseInt(subStr));
+    }
+
+    public double collectPay() {
+        return 1000.00;
+    }
+
+    public String terminate(String endDate) {
+        return (this.name + " ended employment on " + endDate);
+    }
+}
