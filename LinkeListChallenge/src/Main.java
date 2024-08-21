0eab0883aa00d7fef776814c901479a92c2f8397
tmcmac <timmcmackenjr@gmail.com>
2024-08-20 18:11:14 -0700

    Finished the linked list challenge, chapter 141

--- LinkeListChallenge/src/Main.java
+++ LinkeListChallenge/src/Main.java
@@ -6,6 +6,8 @@ public class Main {
     public static void main(String[] args) {
         // Build out itinerary of cities and distances
         LinkedList<Place> placesToVisit = cities();
+        // We need a scanner to take input from the CLI
+        Scanner scanner = new Scanner(System.in);
         // We'll use an iterator to move back and forth through the linked list placesToVisit
         ListIterator<Place> iterator = placesToVisit.listIterator(0);
         // This will be our menu of options
@@ -15,37 +17,65 @@ public class Main {
                 (B)ackward
                 (L)ist
                 (M)enu
-                (Q)uit
-                """;
+                (Q)uit""";
         System.out.println("Welcome to my trip planner app\nTo see the trip we have planned us the following menu.\n");
+        // initial print of menu
         System.out.println(menu);
-        while(true) {
-            // We need a scanner to take input from the CLI
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine().toUpperCase().stripTrailing();
+        // We'll use a bool to keep the while look going until a quit or end of the list
+        boolean keepLoop = true;
+        // we need a forward flag to help us know if the curser/iterator is changing directions
+        boolean forward = true;
+        while(keepLoop) {
+            if(!iterator.hasPrevious()) {
+                Place city = iterator.next();
+                System.out.println("Originating : " + city.getCityName());
+            }
+            if(!iterator.hasNext()) {
+                Place city = iterator.previous();
+                System.out.println("Final : " + city.getCityName());
+                forward = false;
+            }
+            String input = scanner.nextLine().toUpperCase().substring(0,1);
             switch (input) {
-                case "F", "FORWARD":
+                case "F":
+                    System.out.println("User wants to move forward");
+                    if(!forward) { //Reversing direction
+                        forward = true;
+                        if(iterator.hasNext()) {
+                            iterator.next(); // adjust the position forward
+                        }
+                    }
                     if(iterator.hasNext()) {
-                        iterator.next();
-                    } else {
-                        System.out.println("There is no next, it was a great trip!");
+                        Place city = iterator.next();
+                        System.out.println("City : " + city.getCityName() + " distance from SF : " + city.getDistance());
                     }
-                case "B", "BACKWARD":
+                    break;
+                case "B":
+                    System.out.println("User wants to move backward");
+                    if(forward) { //Reversing direction
+                        forward = false;
+                        if(iterator.hasPrevious()) {
+                            iterator.previous(); // adjust the position backward
+                        }
+                    }
                     if(iterator.hasPrevious()) {
-                        iterator.previous();
-                    } else {
-                        System.out.println("There is no previous, we're at the start of a great trip!");
+                        Place city = iterator.previous();
+                        System.out.println("City : " + city.getCityName() + " distance from SF : " + city.getDistance());
                     }
-                case "L", "LIST":
+                    break;
+                case "L":
                     printItinerary(placesToVisit);
-                case "M", "MENU":
+                    break;
+                case "M":
                     System.out.println(menu);
-                case "Q", "QUIT":
+                    break;
+                case "Q":
                     System.out.println("Thanks for checking out the trip!");
-                    return;
+                    keepLoop = false;
+                    break;
                 default:
                     System.out.println("Please select an item from the menu...");
-                    input = "";
+                    break;
             }
         }
 
