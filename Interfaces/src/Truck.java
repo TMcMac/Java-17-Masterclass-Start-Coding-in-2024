26a4cc9b87bfe84de904662a374c08efec2a843e
tmcmac <timmcmackenjr@gmail.com>
2024-09-16 20:30:45 -0700

    Added time stamp to tracked

--- Interfaces/src/Truck.java
+++ Interfaces/src/Truck.java
@@ -2,6 +2,6 @@ public class Truck implements Trackable {
 
     @Override
     public void track() {
-        System.out.println(getClass().getSimpleName() + "'s coordinates recorded");
+        System.out.println(getClass().getSimpleName() + "'s coordinates recorded - " + java.time.ZonedDateTime.now());
     }
 }
